5e58c8de1faf433689a0cdafee139d642889eee8
tamy_le <tamy.le.cs@gmail.com>
2024-04-09 01:48:06 +0900

    :recycle: (optimize) 8th day | two pointers | O(n)

--- 42.trapping-rain-water.py
+++ 42.trapping-rain-water.py
@@ -8,31 +8,30 @@
 # @lc code=start
 class Solution:
     def trap(self, height: List[int]) -> int:
-        if len(height) < 3:
+        h_len = len(height)
+        if h_len < 3:
             return 0
 
-        h_len = len(height)
-        l = 0
-        while l < h_len and height[l] == 0:
-            l += 1
-        r = l + 1
-        previous_max_index = r
-        rain_water = 0
-        while l < r < h_len:
-            while r < h_len - 1 and height[r] <= height[l]:
-                r += 1
-                if height[r] >= height[previous_max_index]:
-                    previous_max_index = r
-
-            rain_water += ((previous_max_index - l) - 1) * min(
-                height[previous_max_index], height[l]
-            ) - sum(height[l + 1 : previous_max_index])
-            l = previous_max_index
-            r = l + 1
-            previous_max_index = r
-        return rain_water
+        max_left, max_right = height[0], height[-1]
+        l, r = 1, h_len - 2
+        ans = 0
+        while l <= r:
+            if max_left < max_right:
+                if max_left < height[l]:
+                    max_left = height[l]
+                else:
+                    ans += max_left - height[l]
+                l += 1
+            else:
+                if max_right < height[r]:
+                    max_right = height[r]
+                else:
+                    ans += max_right - height[r]
+                r -= 1
+            print(l, r, max_left, max_right)
+        return ans
 
 
 # @lc code=end
-# Time: O(n^2)
-# Memory: O(n)
+# Time: O(n)
+# Memory: O(1)
