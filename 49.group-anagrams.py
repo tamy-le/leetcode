a0b58e799b789bb4c5e622af99dbe36b1989fe61
tamy_le <tamy.le.cs@gmail.com>
2024-04-11 01:12:04 +0900

    :zap: (optimize memory) better to return list than dictonary view object

--- 49.group-anagrams.py
+++ 49.group-anagrams.py
@@ -8,16 +8,19 @@
 # @lc code=start
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        sorted_strs = list(map(lambda x: "".join(sorted(x)), strs))
         result = {}
-        for i in range(len(sorted_strs)):
-            if sorted_strs[i] not in result:
-                result[sorted_strs[i]] = [strs[i]]
+        for word in strs:
+            sorted_str = "".join(sorted(word))
+            if sorted_str not in result:
+                result[sorted_str] = [word]
             else:
-                result[sorted_strs[i]].append(strs[i])
+                result[sorted_str].append(word)
         return result.values()
 
 
 # @lc code=end
-# Time: O(n)
-# Memory: O(n)
+# Time: O(n*m*logm)
+# Memory: O(n*m)
+
+# Note: return result.values() use more memory then return list:
+# 1. Because dict used more space then list and return dictionary object view is still keep dict in memory
