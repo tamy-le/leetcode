da97a2bac8f10df1cfea549bdd6d225abafb9bb6
tamy_le <tamy.le.cs@gmail.com>
2024-04-20 17:59:25 +0900

    :zap: (:sparkles: (20th day) array&hash | O(1) | 36: optimize time)

--- 36.valid-sudoku.py
+++ 36.valid-sudoku.py
@@ -1,50 +1,16 @@
+from typing import List
+
+
 class Solution:
     def isValidSudoku(self, board: List[List[str]]) -> bool:
-        box_size = 3
-        board_size = 9
-
-        def canAddToHash(variable, dictionary):
-            return variable not in dictionary or variable == "."
-
-        def containsDuplicate(nums: List[int]) -> bool:
-            hash_dict = {}
-            for i in range(len(nums)):
-                if canAddToHash(nums[i], hash_dict):
-                    hash_dict[nums[i]] = 1
-                else:
-                    return True
-            return False
-
-        def checkHorizontal(board: List[int]) -> bool:
-            for i in range(board_size):
-                if containsDuplicate(board[i]):
-                    return False
-            return True
-
-        def checkVertical(board: List[int]) -> bool:
-            for i in range(board_size):
-                hash_dict = {}
-                for j in range(board_size):
-                    if canAddToHash(board[j][i], hash_dict):
-                        hash_dict[board[j][i]] = 1
-                    else:
-                        return False
-            return True
-
-        def checkBox(board: List[int]) -> bool:
-            for i in range(box_size):
-                for j in range(box_size):
-                    hash_dict = {}
-                    for t in range(i * box_size, (i + 1) * box_size):
-                        for k in range(j * box_size, (j + 1) * box_size):
-                            if canAddToHash(board[t][k], hash_dict):
-                                hash_dict[board[t][k]] = 1
-                            else:
-                                return False
-            return True
-
-        return checkHorizontal(board) and checkVertical(board) and checkBox(board)
+        output = []
+        for i in range(9):
+            for j in range(9):
+                element = board[i][j]
+                if element != ".":
+                    output += [(i, element), (element, j), (i // 3, j // 3, element)]
+        return len(output) == len(set(output))
 
 
-# Time: O(1) -> because we know the actual number of operations
-# Memory: O(1) -> same
+# Time: O(1) -> Optimize time better, less code
+# Memory: O(1)
