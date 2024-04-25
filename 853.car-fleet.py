96eba8e140256580494a2b357199bf71c93b0bbf
tamy_le <tamy.le.cs@gmail.com>
2024-04-26 00:13:05 +0900

    :bug: (24th day) stack | O(nlogn) | 853: fix wrong complexity and wrong converter int

--- 853.car-fleet.py
+++ 853.car-fleet.py
@@ -10,8 +10,8 @@ class Solution:
     def carFleet(self, target: int, position: List[int], speed: List[int]) -> int:
         reach_times_by_pos = list(
             map(
-                lambda x: int((target - x[0]) / x[1]),
-                sorted(zip(position, speed), key=lambda x: -x[0]),
+                lambda x: (target - x[0]) / x[1],
+                sorted(zip(position, speed), reverse=True),
             )
         )
         result = [reach_times_by_pos[0]]
@@ -22,3 +22,5 @@ class Solution:
 
 
 # @lc code=end
+# Time: O(nlogn) -> sort
+# Memory: O(n) -> don't count result
