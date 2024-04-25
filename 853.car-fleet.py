bacebe5b8f892729f781b70c0d3b6eed61bc3098
tamy_le <tamy.le.cs@gmail.com>
2024-04-26 00:23:14 +0900

    :zap: (24th day) sorting | O(nlogn) | 853

--- 853.car-fleet.py
+++ 853.car-fleet.py
@@ -14,13 +14,15 @@ class Solution:
                 sorted(zip(position, speed), reverse=True),
             )
         )
-        result = [reach_times_by_pos[0]]
+        min_reach_time = reach_times_by_pos[0]
+        res = 1
         for reach_time in reach_times_by_pos[1:]:
-            if reach_time > result[-1]:
-                result.append(reach_time)
-        return len(result)
+            if reach_time > min_reach_time:
+                res += 1
+                min_reach_time = reach_time
+        return res
 
 
 # @lc code=end
 # Time: O(nlogn) -> sort
-# Memory: O(n) -> don't count result
+# Memory: O(n) -> don't count result, not the best memory but not in-place function
