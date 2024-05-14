819793674087d9e10d2718c7f1216dcf5a75467a
tamy_le <tamy.le.cs@gmail.com>
2024-05-14 21:10:00 +0900

    :sparkles: (38th day) stack | O(n) | 84

---
+++ 84.largest-rectangle-in-histogram.py
@@ -0,0 +1,31 @@
+#
+# @lc app=leetcode id=84 lang=python3
+#
+# [84] Largest Rectangle in Histogram
+#
+
+
+# @lc code=start
+class Solution:
+    def largestRectangleArea(self, heights: List[int]) -> int:
+        stack = []
+        largest_area = 0
+        len_heights = len(heights)
+        for i in range(len_heights):
+            print(stack, largest_area)
+            index = i
+            while stack and heights[i] < stack[-1][1]:
+                index, height = stack.pop()
+                largest_area = max(largest_area, height * (i - index))
+            stack.append([index, heights[i]])
+
+        for i in range(len(stack)):
+            index, height = stack.pop()
+            largest_area = max(largest_area, (len_heights - index) * height)
+        return largest_area
+
+
+# Time: O(n)
+# Memory: O(n) if heights in acs order
+
+# @lc code=end
